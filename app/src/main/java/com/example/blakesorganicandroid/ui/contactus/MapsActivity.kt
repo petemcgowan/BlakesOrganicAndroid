775b95872dcf2ca42ae71cba1e71533d55858cda
Pete <peteburquette@gmail.com>
2022-09-25 18:36:19 +0100

    Google Maps, collating markers

--- app/src/main/java/com/example/blakesorganicandroid/ui/contactus/MapsActivity.kt
+++ app/src/main/java/com/example/blakesorganicandroid/ui/contactus/MapsActivity.kt
@@ -48,6 +48,126 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val shopStGalway = LatLng(53.324722735026704, -9.068437264395666)
         mMap.addMarker(MarkerOptions().position(shopStGalway).title("Shop St Galway"))
 
+//        Rua Cafe and Delicatessen
+//        53.854600123239756, -9.296694191984217
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+
+
+
+
+        // McCambridges, Galway
+        53.2732505412702, -9.05247730628975
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Fresh, Carrick On Shannon
+        53.96451231067534, -8.103642109125982
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Shannon Fruit, Tullyvarraga
+        52.762242561973466, -8.890905442532103
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Cosgrove & Son, Abbeyquarter North
+        54.3115881661309, -8.48392233856201
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Osta, Sligo
+        54.308048664833024, -8.475682584134434
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Nature Health, Navan
+        53.740982819774814, -6.684484786058696
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Natures Gold, Dunboyne
+        53.51819675385799, -6.50351556413804
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Nature’s Gold, Rathcoole
+        53.35865936381066, -6.470818426724693
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Fresh, Rathfarnham
+        53.37832487715894, -6.3209811838714245
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Boyne Valley Seafoods, Drogheda
+        53.750632310076284, -6.316059084892225
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // The Hopsack, Rathmines
+        53.41108056215478, -6.299161419926029
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Fresh, IFSC Dublin
+        53.44836580465253, -6.288247353275163
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Donnybrook Fair, Dublin 4
+        53.42417577807169, -6.259117650199059
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Georges Fish Store, Stepaside
+        53.338984771408164, -6.243972400199067
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Donnybrook Fair, Stillorgan
+        53.36521554361809, -6.213881644417493
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Select Store, Dalkey
+        53.355380895617344, -6.0752566573458004
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Nash 19 Restaurant, Cork
+        51.99171950558138, -8.45278680100184
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Manna Organic Store, Island of Geese, Kerry
+        52.269798115324036, -9.71046353172283
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Simple Simons, Donegal
+        54.65393311182183, -8.107479944173482
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Between The Bridges, Enniskillen
+        54.344555675048454, -7.638029229826629
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Fresh, Smithfield, Dublin
+        53.34946379011656, -6.278325172177447
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+        // Morton's, Ranelagh, Dublin
+        53.32118879912855, -6.2561448351287225
+        val nameOfLocation = LatLng(0.0, 0.0)
+        mMap.addMarker(MarkerOptions().position(nameOfLocation).title("TITLE"))
+
+
         // Move the camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(navenny))
         mMap.moveCamera(CameraUpdateFactory.zoomTo(5f))
---
+++ docs/Stockists Locations, Google Maps longtitude latitude.md
@@ -0,0 +1,108 @@
+Rua Cafe and Delicatessen
+Spencer St, Gorteendrunagh, Castlebar, Co. Mayo
+53.854600123239756, -9.296694191984217
+
+McCambridge’s of Galway
+38/39 Shop Street
+Galway
+53.2732505412702, -9.05247730628975
+
+Fresh Today- Carrick On Shannon
+7 Landmark Court
+Carrick On Shannon
+53.96451231067534, -8.103642109125982
+
+Shannon Fruit & Veg
+Skycourt Shopping Centre
+Tullyvarraga, Shannon, Co. Clare
+52.762242561973466, -8.890905442532103
+
+M Cosgrove & Son Delicatessen Food Shop
+32 Market St, Abbeyquarter North
+Sligo
+54.3115881661309, -8.48392233856201
+
+Osta Cafe & Wine Bar
+Garavogue Weir
+off Stephen Street
+Sligo
+54.308048664833024, -8.475682584134434
+
+Nature Health Food Store
+Navan Shoppping Cantre
+1 kennedy Rd.
+Navan C15ncd3
+53.740982819774814, -6.684484786058696
+
+Nature’s Gold
+Avoca
+Dunboyne
+53.51819675385799, -6.50351556413804
+
+Nature’s Gold
+Avoca Stores & cafe
+Naas Road
+Rathcoole
+53.35865936381066, -6.470818426724693
+
+Get Fresh – Rathfarnham
+Unit 6, Rosemount Shopping Centre
+Marian Road
+Rathfarnham
+53.37832487715894, -6.3209811838714245
+
+Boyne Valley Seafoods
+Unit 11A Southgate Shopping Centre
+Dublin Road
+Drogheda
+53.750632310076284, -6.316059084892225
+
+The Hopsack
+The Swan Shopping Centre
+Rathmines Road Lower
+53.41108056215478, -6.299161419926029
+
+Fresh IFSC Mayor Street
+Mayor Street Lower
+Dublin
+53.44836580465253, -6.288247353275163
+
+Donnybrook Fair-Morehampton Rd
+89 Morehampton Rd
+Dublin 4
+53.42417577807169, -6.259117650199059
+
+George’s Fish Store – Stepaside
+18 Enniskerry Road
+Stepaside
+53.338984771408164, -6.243972400199067
+
+Donnybrook Fair- Stillorgan
+47-49,Lower Kilmacud Road
+Stillorgan Shopping Centre
+53.36521554361809, -6.213881644417493
+
+Select Stores – Dalkey
+1 Railway Rd
+Dalkey
+53.355380895617344, -6.0752566573458004
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
