f344ffc5b7a3a8ab6edef1627cd13d8b05871318
Pete <peteburquette@gmail.com>
2022-09-26 02:23:03 +0100

    Description order

--- app/src/main/java/com/example/blakesorganicandroid/ui/benefits/BenefitsFragment.kt
+++ app/src/main/java/com/example/blakesorganicandroid/ui/benefits/BenefitsFragment.kt
@@ -55,7 +55,32 @@ class BenefitsFragment : Fragment(), BookClickListener {
     {
         val benefit1 = Book(
             R.drawable.benefit1_kefir_nutrients,
-            "longDescription",
+            "Kefir originated from parts of Eastern Europe and Southwest Asia. Its name comes from the Turkish word “keyif,” which refers to feeling good after eating.\n" +
+                    "\n" +
+                    "This is a fermented drink, traditionally made with cow’s milk or goat’s milk.\n" +
+                    "\n" +
+                    "It is made by adding kefir grains to the milk. These are not cereal grains, but grain-like colonies of yeast and lactic acid bacteria that resemble a cauliflower in appearance.\n" +
+                    "\n" +
+                    "Over approximately 24 hours, the microorganisms in the kefir grains multiply and ferment the sugars in the milk, turning it into kefir. Then, the grains are removed from the liquid and can be used again.\n" +
+                    "\n" +
+                    "In other words, kefir is the drink, but kefir grains are the starter culture that used to produce the beverage.\n" +
+                    "\n" +
+                    "The grains’ lactic acid bacteria turn the milk’s lactose into lactic acid. This is what makes kefir taste sour like yogurt — but it has a thinner consistency.\n" +
+                    "\n" +
+                    "A 1 cup serving of low fat kefir contains:\n" +
+                    "\n" +
+                    "Protein: 9 grams\n" +
+                    "Calcium: 24% of the daily value (DV)\n" +
+                    "Phosphorus: 20% of the DV\n" +
+                    "Vitamin B12: 29% of the DV\n" +
+                    "Riboflavin (B2): 25% of the DV\n" +
+                    "Magnesium: 7% of the DV\n" +
+                    "Vitamin D: 12% of the DV\n" +
+                    "In addition, kefir has about 104 calories, 11.6 grams of carbs, and 2–3 grams of fat, depending on the type of milk used.\n" +
+                    "\n" +
+                    "Kefir also contains a wide variety of bioactive compounds, including organic acids and peptides that contribute to its health benefits.\n" +
+                    "\n" +
+                    "Dairy-free versions of kefir can be made with coconut water, coconut milk, or other sweet liquids. These will not have the same nutrient profile as dairy-based kefir.",
             "Kefir is a fantastic source of many nutrients",
             "Kefir originated from parts of Eastern Europe and Southwest Asia. Its name comes from the Turkish word “keyif,” which refers to feeling good after eating.\n" +
                     "\n" +
@@ -89,7 +114,18 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit2 = Book(
             R.drawable.benefit2_probiotics_gut_microbiome,
-            "longDescription",
+            "Some microorganisms can have beneficial effects on health when ingested.\n" +
+                    "\n" +
+                    "Known as probiotics, these microorganisms may influence health in numerous ways, aiding digestion, weight management, and mental health.\n" +
+                    "\n" +
+                    "Yogurt is the best-known probiotic food in the Western diet, but kefir is actually a much more potent source.\n" +
+                    "\n" +
+                    "Kefir grains contain up to 61 strains of bacteria and yeasts, making them a very rich and diverse probiotic source, though this diversity may vary.\n" +
+                    "\n" +
+                    "Other fermented dairy products are made from far fewer strains and don’t contain any yeasts.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Kefir may contain up to 61 different microorganisms, making it a much more potent source of probiotics than many other fermented dairy products",
             "Kefir is a more powerful probiotic than yogurt",
             "Some microorganisms can have beneficial effects on health when ingested.\n" +
                     "\n" +
@@ -109,7 +145,16 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit3 = Book(
             R.drawable.benefit3_bacteria_allv1,
-            "longDescription",
+            "Certain probiotics in kefir are believed to protect against infections.\n" +
+                    "\n" +
+                    "This includes the probiotic Lactobacillus kefiri, which is unique to kefir.\n" +
+                    "\n" +
+                    "Studies demonstrate that this probiotic can inhibit the growth of various harmful bacteria, including Salmonella, Helicobacter pylori, and E. coli.\n" +
+                    "\n" +
+                    "Kefiran, a type of carbohydrate present in kefir, also has antibacterial properties.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Kefir contains the probiotic Lactobacillus kefiri and the carbohydrate kefiran, both of which protect against harmful bacteria.",
             "Kefir has potent antibacterial properties\n",
             "Certain probiotics in kefir are believed to protect against infections.\n" +
                     "\n" +
@@ -127,7 +172,12 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit4 = Book(
             R.drawable.benefit4_healthy_bones_skeleton,
-            "longDescription",
+            "Osteoporosis is characterized by deterioration of bone tissue and is a major concern in Western countries. It is especially common among older women and dramatically raises the risk of fractures.\n" +
+                    "It is, however, more probable that what kefir can do is that it can heal the gut and in turn a healthy gut can ward of many diseases.\n" +
+                    "Ensuring an adequate calcium intake is one of the most effective ways to improve bone health and slow the progression of osteoporosis." +
+                    "Full fat kefir is not only a great source of calcium but also vitamin K2 — which plays a central role in calcium metabolism. Supplementing with K2 has been shown to reduce your risk of fractures by as much as 81%" +
+                    "Recent animal studies associate kefir with increased calcium absorption in bone cells. This leads to improved bone density, which should help prevent fractures\n" +
+                    "Tl;dr\n" + "Kefir made from dairy is an excellent source of calcium, and full-fat dairy kefir also contains vitamin K2. These nutrients have major benefits for bone health.",
             "Kefir can improve bone health and lower the risk of osteoporosis",
             "Osteoporosis is characterized by deterioration of bone tissue and is a major concern in Western countries. It is especially common among older women and dramatically raises the risk of fractures.\n" +
                     "It is, however, more probable that what kefir can do is that it can heal the gut and in turn a healthy gut can ward of many diseases.\n" +
@@ -141,7 +191,18 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit5 = Book(
             R.drawable.benefit5_cancer_anti_oxidant_fermentation,
-            "longDescription",
+            "Cancer is one of the world’s leading causes of death. It occurs when abnormal cells in your body grow uncontrollably, such as in a tumor.\n" +
+                    "\n" +
+                    "The probiotics in fermented dairy products are believed to reduce tumor growth by stimulating your immune system. So, it is possible that kefir may fight cancer.\n" +
+                    "\n" +
+                    "This protective role has been demonstrated in several test-tube studies.\n" +
+                    "\n" +
+                    "One study found that kefir extract reduced the number of human breast cancer cells by 56%, compared with only 14% for yogurt extract.\n" +
+                    "\n" +
+                    "Keep in mind that human studies are needed before firm conclusions can be made.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Some test-tube and animal studies indicate that kefir can inhibit cancer cell growth. However, no current studies have included people, so more research is needed.",
             "Kefir may be protective against cancer",
             "Cancer is one of the world’s leading causes of death. It occurs when abnormal cells in your body grow uncontrollably, such as in a tumor.\n" +
                     "\n" +
@@ -161,7 +222,18 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit6 = Book(
             R.drawable.benefit6_good_digestive_system,
-            "longDescription",
+            "Probiotics such as kefir can help restore the balance of friendly bacteria in your gut.\n" +
+                    "\n" +
+                    "This is why they are highly effective at treating many forms of diarrhea.\n" +
+                    "\n" +
+                    "What’s more, ample evidence suggests that probiotics and probiotic foods can alleviate many digestive concerns\n" +
+                    "\n" +
+                    "These include irritable bowel syndrome, ulcers caused by H. pylori infection, and many others.\n" +
+                    "\n" +
+                    "For this reason, kefir may be useful if you have trouble with digestion.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Probiotics like kefir can treat several forms of diarrhea. They can also lead to improvements in various digestive conditions.",
             "Kefir’s probiotics may help with various digestive concerns",
             "Probiotics such as kefir can help restore the balance of friendly bacteria in your gut.\n" +
                     "\n" +
@@ -181,7 +253,20 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit7 = Book(
             R.drawable.benefit7_cow_low_lactose,
-            "longDescription",
+            "Regular dairy foods contain a natural sugar called lactose.\n" +
+                    "\n" +
+                    "Many people, especially adults, are unable to break down and digest lactose properly. This condition is called lactose intolerance (25).\n" +
+                    "\n" +
+                    "The lactic acid bacteria in fermented dairy foods — like kefir and yogurt — turn the lactose into lactic acid, so these foods are much lower in lactose than milk.\n" +
+                    "\n" +
+                    "They also contain enzymes that can help break down the lactose even further.\n" +
+                    "\n" +
+                    "That’s why kefir is generally well tolerated by people with lactose intolerance, at least compared with regular milk.\n" +
+                    "\n" +
+                    "Keep in mind that it is possible to make kefir that is 100% lactose-free by using coconut water, fruit juice, or another nondairy beverage.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Kefir is low in lactose because its lactic acid bacteria have already pre-digested the lactose. People who have lactose intolerance can often drink kefir without problems.",
             "Kefir is low in lactose",
             "Regular dairy foods contain a natural sugar called lactose.\n" +
                     "\n" +
@@ -203,7 +288,16 @@ class BenefitsFragment : Fragment(), BookClickListener {
 
         val benefit8 = Book(
             R.drawable.benefit8_asthma_great_lungs,
-            "longDescription",
+            "Allergic reactions are caused by inflammatory responses against certain foods or substances.\n" +
+                    "\n" +
+                    "People with an oversensitive immune system are more prone to allergies, which can provoke conditions like asthma.\n" +
+                    "\n" +
+                    "In animal studies, kefir has been shown to suppress inflammatory responses related to allergies and asthma.\n" +
+                    "\n" +
+                    "Human studies are needed to better explore these effects.\n" +
+                    "\n" +
+                    "Tl;dr\n" +
+                    "Limited evidence from animal studies suggests that drinking kefir may reduce allergic reactions.",
             "Kefir may improve allergy and asthma symptoms",
             "Allergic reactions are caused by inflammatory responses against certain foods or substances.\n" +
                     "\n" +
--- app/src/main/java/com/example/blakesorganicandroid/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/blakesorganicandroid/ui/history/HistoryFragment.kt
@@ -59,63 +59,63 @@ class HistoryFragment : Fragment(), BookClickListener {
     {
         val book1 = Book(
             R.drawable.history_lough_leitrim_blue,
-            "Victoria Devine",
-            "The company is focused only on certified Organic produce.",
             "Blake’s Always Organic is based in The Food Hub, Drumshanbo, Co. Leitrim since 2012",
+            "The company is focused only on certified Organic produce.",
+            "Desc",
             "H1"
         )
         historyList.add(book1)
 
         val book2 = Book(
             R.drawable.history_lough_leitrim_boardwalk,
-            "Amanda Lohrey",
-            "Sean McGloin became aware of the health benefits of the fermented milk kefir and was making kefir for his family before developing the product on a small commercial scale.",
             "Our current products are Organic Kefir, (a probiotic health drink for gut revitalisation made with whole organic milk.) and Whole-bean Artisan Organic coffees.\n",
+            "Sean McGloin became aware of the health benefits of the fermented milk kefir and was making kefir for his family before developing the product on a small commercial scale.",
+            "desc",
             "H2"
         )
         historyList.add(book2)
 
         val book3 = Book(
             R.drawable.history_lough_leitrim_small,
-            "M. Scott Peck",
-            "Hundreds of years ago kefir was found high in the Caucasus mountains villages of nomadic shepherds who had abnormally good health.",
             "Kefir originates in the Caucasus mountains.",
+            "Hundreds of years ago kefir was found high in the Caucasus mountains villages of nomadic shepherds who had abnormally good health.",
+            "Desc",
             "H3"
         )
         historyList.add(book3)
 
         val book4 = Book(
             R.drawable.history_glencar_waterfall,
-            "Colleen Hoover",
-            "On further research, they discovered that part of the diet was a milk product that was stored in bags or containers made from calf stomachs.",
             "While half the world was dying of malnutrition and disease, these shepherds and their families lived long healthy lives with little or no disease.",
+            "On further research, they discovered that part of the diet was a milk product that was stored in bags or containers made from calf stomachs.",
+            "Desc",
             "H4"
         )
         historyList.add(book4)
 
         val book5 = Book(
             R.drawable.blakes_culture_blend_organic_whole_bean_coffee_catering_pack_1kg,
-            "Ross Coulthart",
-            "In 2016, Blakes Always Organic commenced selling “Feel Good” Organic Kefir.",
             "As temperatures rose the natural bacteria in the wall of the calf’s stomach bag impregnated the milk causing fermentation resulting in billions of good bacteria that was regular drank by the shepherds to achieve good health.",
+            "In 2016, Blakes Always Organic commenced selling “Feel Good” Organic Kefir.",
+            "Desc",
             "H5"
         )
         historyList.add(book5)
 
         val book6 = Book(
             R.drawable.blakes_culture_blend_organic_whole_bean_coffee_catering_pack_1kg,
-            "Richard Osman",
-            "We had to move to a 200 litre vat within 3 months and in 2018 we had to purchase a 850 litre vat.",
             "Starting off, we made batches of kefir in a small 20 litre Bain Marie, which we bottled and labelled by hand.",
+            "We had to move to a 200 litre vat within 3 months and in 2018 we had to purchase a 850 litre vat.",
+            "Desc",
             "H6"
         )
         historyList.add(book6)
 
         val book7 = Book(
             R.drawable.blakes_culture_blend_organic_whole_bean_coffee_catering_pack_1kg,
-            "Michael Robotham",
-            "Three years later, and our production scale has grown continuously to cater for the increasing demand as the undisputed health benefits of milk kefir is the main driver of sales.",
             "We currently produce around 1600 litres per week. The product proved to be very popular with our health conscious customers.\n",
+            "Three years later, and our production scale has grown continuously to cater for the increasing demand as the undisputed health benefits of milk kefir is the main driver of sales.",
+            "Desc",
             "H7"
         )
         historyList.add(book7)
